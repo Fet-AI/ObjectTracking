db48fb9e65be532dc37a3436f33bf694824a1637
SangKyoungPark <tltmzh3@gmail.com>
2025-03-10 23:51:21 +0900

    [Add] - Gaussian Blur Function define

--- ObjectTracking/ObjectTracking/ImageProcessor.cpp
+++ ObjectTracking/ObjectTracking/ImageProcessor.cpp
@@ -37,3 +37,16 @@ bool CImageProcessor::ToColor(std::shared_ptr<cv::Mat> image, std::string& error
         return false;
     }
 }
+
+/*
+* bref 가우시안 블러 적용(노이즈 감소)
+@ cv::GaussianBlur: 가우시안 필터를 사용하여 이미지에 블러를 적용
+@ gray: 입력 그레이스케일 이미지
+@ blurred: 블러가 적용된 이미지
+@ cv::Size(5, 5): 가우시안 커널의 크기 (5x5)
+@ 0: 가우시안 커널의 표준 편차 (0이면 자동 계산)
+*/
+bool CImageProcessor::ToBlur_Gaussian(std::shared_ptr<cv::Mat> image, cv::Mat gray, cv::Size size, int nStandardDeviation, std::string& errorMsg)
+{
+    return false;
+}
--- ObjectTracking/ObjectTracking/ImageProcessor.h
+++ ObjectTracking/ObjectTracking/ImageProcessor.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "ImageModel.h"
 #include <string>
+#include <algorithm>
 
 class CImageProcessor 
 {
 public:
     static bool ToGray(std::shared_ptr<cv::Mat> image, std::string& errorMsg);
     static bool ToColor(std::shared_ptr<cv::Mat> image, std::string& errorMsg);
+    static bool ToBlur_Gaussian(std::shared_ptr<cv::Mat> image,cv::Mat gray, cv::Size size,int nStandardDeviation ,std::string& errorMsg);
 };
